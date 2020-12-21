25f471ccc91e48eb9036fd8762f797dcd7ded3fb
kilka-rav <zakirianov.rv@phystech.edu>
2020-12-22 00:13:38 +0300

    add final version

--- stack/stack.c
+++ stack/stack.c
@@ -1,131 +1,247 @@
 #include "stack.h"
 
+const char path[] = "/home/ravil/3sem/operator/hometask_2/Hometask_3sem/stack";
 
-int global_wait = 0; // 1 - wait, 0 - run
+int timeout_flag = 0;
+struct timespec timeout_time = {0, 0};
 
-
-void message_error(int error) {
-    if ( error < 0 ) {
-        ERROR(strerror(errno));
-        exit(1);
+Stack_t* attach_stack(key_t key, int size) {
+    Stack_t* stack = (Stack_t*)calloc(1, sizeof(Stack_t));
+    if ( stack == NULL ) {
+        ERROR("ERROR IN ATTACH");
+        return stack;
     }
-}
 
-stack_t* attach_stack(key_t key, int size) {
-    stack_t* stack = (stack_t*) malloc(sizeof(stack_t));
-    if ( !stack ) {
-        return NULL;
+    void* memory = NULL; 
+    int shmem_id = shmget(key, size, IPC_EXCL | 0666);
+    int sem_id;
+    if ( shmem_id > 0 ) {
+        memory = shmat(shmem_id, NULL, 0);
+        if ( memory == NULL ) {
+            ERROR("ERROR");
+        }
+        sem_id = semget(key, 2, IPC_EXCL | 0666);
+        if ( sem_id <= 0 ) {
+            ERROR("ERROR IN SEMGET");
+            free(stack);
+            return NULL;
+        }
     }
-    int id = shmget(key, size * sizeof(void*) + 3 * sizeof(int), IPC_CREAT | IPC_EXCL | 0666);
-    if ( (id < 0) && (errno != EEXIST) ) {
-        ERROR("Error in allocation");
-        message_error(id);
-        exit(2);
+    else {
+        shmem_id = shmget(key, size, IPC_CREAT | IPC_EXCL | 0666);
+        check(shmem_id);
+        memory = shmat(shmem_id, NULL, 0);
+        sem_id = semget(key, 2, IPC_CREAT | IPC_EXCL | 0666);
+        check(sem_id);
+        sem_change(sem_id, 1, 1, 0);
     }
-    int sema = semget(key, 1, IPC_CREAT | IPC_EXCL | 0777);
-    //message_error(sema);
-    void* mem = shmat(id, NULL, 0);
-    init_sema(0);
-    increment_sem(sema);
-    stack->key = key;
-    stack->data[0] = size;
-    stack->data[1] = 0;
-    stack->data[2] = sema;
-    stack->mem = mem;
-    printf("size: %d\n", stack->data[0]);
+
+    stack->size = size;
+    stack->memory = memory;
+    stack->shmem_id = shmem_id;
+    stack->sem_id = sem_id;
     return stack;
 }
 
-int detach_stack(stack_t* stack) {
-    int reply = shmdt((void*)stack->mem);
-    message_error(reply);
-    return reply;
+int detach_stack(Stack_t* stack) {
+    if ( stack == NULL ) {
+        ERROR("NOT STACK");
+        return -1;
+    }
+    else {
+        int shm_del = shmdt(stack->memory);
+        check(shm_del);
+        return 0;
+    }
 }
 
-int push(stack_t* stack, void* val) {
-    decrement_sem(stack->data[3]);
-    if ( stack->data[0] == stack->data[1] ) {
+int mark_destruct(Stack_t* stack) {
+    if ( stack == NULL ) {
+        ERROR("NOT_STACK");
+        return -1;
+    }
+    else {
+        int shm_del = shmctl(stack->shmem_id, IPC_RMID, NULL);
+        check(shm_del);
+        shm_del = semctl(stack->sem_id, 0, IPC_RMID);
+        check(shm_del);
+        return 0;
+    }
+}
+
+int get_size(Stack_t* stack) {
+    if ( stack == NULL ) {
+        return -1;
+    }
+    else {
+        return stack->size;
+    }
+}
+
+int get_count(Stack_t* stack) {
+    if ( stack == NULL ) {
+        return -1;
+    }
+    else {
+        int val = semctl(stack->sem_id, 0, GETVAL);
+        return val;
+    }
+}
+
+int push(Stack_t* stack, void* val) {
+    if ( stack == NULL ) {
+        ERROR("NOT STACK");
+        return -1;
+    }
+    if ( get_count(stack) >= get_size(stack) ) {
         ERROR("STACK IS OVERLOAD");
         return -1;
     }
-    stack->mem[stack->data[1]] = val;
-    stack->data[1]++;
-    increment_sem(stack->data[3]);
-    return 0;
+    int waiting = sem_change(stack->sem_id, 1, -1, 1);
+    if ( waiting == 0 ) {
+        stack->memory[get_count(stack)] == val;
+        sem_change(stack->sem_id, 0, 1, 0);
+        sem_change(stack->sem_id, 1, 1, 1);
+        return 0;
+    }
+    return -1;
 }
 
-int pop(stack_t* stack, void** val) {
-    decrement_sem(stack->data[3]);
-    if ( stack->data[1] <= 0 ) {
-        *val = NULL;
-        ERROR("STACK IS EMPTY");
+int pop(Stack_t* stack, void** val) {
+    if ( stack == NULL ) {
+        ERROR("NOT STACK");
         return -1;
     }
-    //add handler
-    *val = stack->mem[stack->data[1] - 1];
-    stack->data[1]--;
-    increment_sem(stack->data[3]);
-    return 0;
-}
 
-int mark_destruct(stack_t* stack) {
-    int id = shmget(stack->key, stack->data[0] * sizeof(void*), 0);
-    message_error(id);
-    int check = semctl(stack->data[2], 1, IPC_RMID);
-    message_error(id);
-    int reply = shmctl(id, IPC_RMID, NULL);
-    message_error(reply);
-    return reply;
-}
-
-
-
-
-int get_size(stack_t* stack) {
-    return stack->data[0];
-}
-
-int get_count(stack_t* stack) {
-    return stack->data[1];
-}
-
-Sembuf* init_sema(int num) {
-    static Sembuf semafor;
-    if ( num == 0 ) {
-        semafor.sem_num = num; //next SIGSEGV
-        if ( global_wait == 1 ) {
-            semafor.sem_flg = SEM_UNDO;
+    int waiting = sem_change(stack->sem_id, 1, -1, 1);
+    if ( waiting == 0 ) {
+        if ( get_count(stack) == 0 ) {
+            ERROR("STACK IS EMPTY");
+            sem_change(stack->sem_id, 1, 1, 1);
+            return -1;
         }
-        else if ( global_wait == 0 ) {
-            semafor.sem_flg = IPC_NOWAIT | SEM_UNDO;
+        *val = stack->memory[get_count(stack) - 1];
+        sem_change(stack->sem_id, 0, -1, 0);
+        sem_change(stack->sem_id, 1, 1, 1);
+        return 0; 
+    }
+    return -1;    
+}
+
+int set_wait(int val, struct timespec* timeout) {
+    timeout_flag = val;
+    if ( timeout != NULL ) {
+        timeout_time.tv_nsec = timeout->tv_nsec;
+        timeout_time.tv_sec = timeout->tv_sec;
+    }
+}
+
+int sem_change(int sem_id, int sem_num, int val, int undo) {
+    int res;
+    struct sembuf sems;
+    sems.sem_num = sem_num;
+    sems.sem_op = val;
+
+    if ( sem_num == 0 ) {
+        sems.sem_flg = 0;
+        return semop(sem_id, &sems, 1);
+    }
+    else if ( sem_num == 1 ) {
+        if (timeout_flag == -1) {
+            if (undo == 1) {
+                sems.sem_flg = IPC_NOWAIT | SEM_UNDO;
+            }
+            else { 
+                sems.sem_flg = IPC_NOWAIT;
+            }
+            res = semop(sem_id, &sems, 1);
+            return res; 
         }
-        else {
-            fprintf(stderr, "ERROR IN SEMA\n");
-            exit(3);
+        else if (timeout_flag == 0) {
+            if ( undo == 1 ) {
+                sems.sem_flg = SEM_UNDO;
+            }
+            else {
+                sems.sem_flg = 0;
+            }
+            res = semop(sem_id, &sems, 1);
+            return res; 
         }
+        else if ( timeout_flag == 1 ) {
+            if ( undo == 1 ) {
+                sems.sem_flg = SEM_UNDO;
+            }
+            else {
+                sems.sem_flg = 0;
+            }
+            res = semtimedop(sem_id, &sems, 1, &timeout_time);
+            return res; 
+        } 
     }
-    else {
-        semafor.sem_num = num;
-    }
-    return &semafor;
+    return -1;
 }
 
-int increment_sem(int sem) {
-    if ( global_wait = 1 ) {
-        return semtimedop(sem, init_sema(1), 1, NULL); 
+void unit_test2(key_t key) {
+    const int size = 1000000;
+    const int push_count = 10;
+    const int fork_count = 11;
+    pid_t main_pid = getpid();
+    pid_t pid;
+    setgid(main_pid);
+    for (int i = 0; i < 2; i++)
+    {
+        pid = fork();
     }
-    else {
-        return semop(sem, init_sema(1), 1);
+
+    Stack_t* stack = attach_stack(key, size);
+    for (int j = 0; j < push_count; j++) {
+        void* a = NULL + j + pid * 5;
+        int check = push(stack, a);
+        printf("count == %d size = %d\n", get_count(stack), get_size(stack));
+    }
+    for (int j = 0; j < push_count; j++) {
+        void* a;
+        int check = pop(stack, &a);
+    }
+    if (getpid() == main_pid) {
+        void* a;
+        sleep(1);
+        setgid(main_pid + 1);
+        killpg(main_pid, SIGKILL);
+        push(stack, NULL);
+        push(stack, NULL);
+        pop(stack, &a);
+        pop(stack, &a);
+        printf("TEST 3 =  %d\n", get_count(stack));
+        detach_stack(stack);
+        mark_destruct(stack);
+    }
+    else
+    {
+        printf("end of the child with pid %d\n", getpid());
+        detach_stack(stack);
     }
 }
-
-int decrement_sem(int sem) { 
-    if ( global_wait == 1 ) {
-        return semtimedop(sem, init_sema(-1), 1, NULL);
+void unit_test1(key_t key) {
+    printf("UNIT_TES1\nRUN\n_________________________________\n");
+    Stack_t* s = attach_stack(key, 5);
+    for(size_t i = 0; i < 5; i++) {
+        printf("count %d\n", get_count(s));
+        printf("size %d\n", get_size(s));
+        push(s, (void*)(i+1));
     }
-    else {
-        return semop(sem, init_sema(-1), 1);
+    for(size_t j = 0; j < 6; j++){
+        void* val = NULL;
+        pop(s, &val);
+        printf("%p\n", val);
     }
+    int a = detach_stack(s);
+    mark_destruct(s);
+    printf("%d\n_______________________________________________\nEND\n", a);
 }
 
-
+int main(int argc, char** argv) {
+    key_t key = atoi(argv[1]);
+    unit_test1(key);
+    unit_test2(key + 2);
+    }
--- stack/stack.h
+++ stack/stack.h
@@ -1,54 +1,62 @@
+#ifndef _STACK_H_
+#define _STACK_H_
+
 #define _GNU_SOURCE
 #include <stdio.h>
-#include <time.h>
-#include <string.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <sys/sem.h>
 #include <stdlib.h>
+#include <sys/shm.h>
+#include <assert.h>
+#include <sys/sem.h>
+#include <sys/stat.h>
+#include <sys/ipc.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <unistd.h>
+#include <time.h>
 #include <errno.h>
+#include <string.h>
+#include <signal.h>
 
+#define ERROR(string) fprintf(stderr, "%s\nLINE = %d\n", string, __LINE__)
 
-#define ERROR(string) fprintf(stderr, "%s\n", string)
-typedef struct sembuf Sembuf;
+typedef struct _Stack_t {
+    int size;
+    void** memory;
+    int shmem_id;
+    int sem_id;
+} Stack_t;
 
-typedef struct _stack_t {
-    void** mem;
-    key_t key;
-    int data[3]; //data[0] - size, data[1] - count, data[2] - sem
-    //int size;
-    //int count;
-    //int sem;
-} stack_t;
-
+void check(int arg) {
+    if ( arg < 0 ) {
+        ERROR(strerror(errno));
+        exit(1);
+    }
+}
 /* Attach (create if needed) shared memory stack to the process.
-Returns stack_t* in case of success. Returns NULL on failure. */
-stack_t* attach_stack(key_t key, int size);
+Returns Stack_t* in case of success. Returns NULL on failure. */
+Stack_t* attach_stack(key_t key, int size);
 
 /* Detaches existing stack from process. 
 Operations on detached stack are not permitted since stack pointer becomes invalid. */
-int detach_stack(stack_t* stack);
+int detach_stack(Stack_t* stack);
 
 /* Marks stack to be destroed. Destruction are done after all detaches */ 
-int mark_destruct(stack_t* stack);
+int mark_destruct(Stack_t* stack);
 
 /* Returns stack maximum size. */
-int get_size(stack_t* stack);
+int get_size(Stack_t* stack);
 
 /* Returns current stack size. */
-int get_count(stack_t* stack);
+int get_count(Stack_t* stack);
 
 /* Push val into stack. */
-int push(stack_t* stack, void* val);
+int push(Stack_t* stack, void* val);
 
 /* Pop val from stack into memory */
-int pop(stack_t* stack, void** val);
+int pop(Stack_t* stack, void** val);
 
-Sembuf* init_sema(int num);
-int increment_sem(int sem);
-int decrement_sem(int sem);
+/* Change semaphore */
+int sem_change(int sem_id, int sem_num, int val, int undo);
 
 //---------------------------------------------
 /* Additional tasks */
@@ -58,4 +66,6 @@ val == -1 Operations return immediatly, probably with errors.
 val == 0  Operations wait infinitely.
 val == 1  Operations wait timeout time.
 */
-//int set_wait(int val, timespec* timeout);
+int set_wait(int val, struct timespec* timeout);
+
+#endif
